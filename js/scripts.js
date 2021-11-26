d61971ecebdac9e38b03ee443a49bf7c86a6f9d8
mariyatur27 <63456223+mariyatur27@users.noreply.github.com>
2021-11-25 22:06:48 -0500

    new changes, still not fully working

--- js/scripts.js
+++ js/scripts.js
@@ -153,3 +153,33 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
 }
+
+var iframe = top.frames[name].document;
+var css = '' +
+          '<style type="text/css">' +
+          'body{margin:0;padding:0;background:transparent}' +
+          '</style>';
+iframe.open();
+iframe.write(css);
+iframe.close();
+
+// Slide Show Code
+// var slideIndex = 0;
+// showSlides();
+
+// function showSlides() {
+//   var i;
+//   var slides = document.getElementsByClassName("carousel_item");
+//   var dots = document.getElementsByClassName("dot");
+//   for (i = 0; i < slides.length; i++) {
+//     slides[i].style.display = "none";  
+//   }
+//   slideIndex++;
+//   if (slideIndex > slides.length) {slideIndex = 1}    
+//   for (i = 0; i < dots.length; i++) {
+//     dots[i].className = dots[i].className.replace(" active", "");
+//   }
+//   slides[slideIndex-1].style.display = "block";  
+//   dots[slideIndex-1].className += " active";
+//   setTimeout(showSlides, 5000); // Change image every 2 seconds
+// }
