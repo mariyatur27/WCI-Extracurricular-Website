3f033174eeff022b7b917b34dd7d9b9891d3b95f
mariyatur27 <63456223+mariyatur27@users.noreply.github.com>
2021-12-02 20:20:41 -0500

    Making a functional search bar for music
    Tristan please don't make this code more complicated

--- js/scripts.js
+++ js/scripts.js
@@ -70,6 +70,22 @@ function search_club() {
     }
 };
 
+// Music search bar
+function search_music() {
+    var input = document.getElementById('club_search').value;
+    input = input.toLowerCase();
+    var music = document.getElementsByClassName('orch-head');
+    var music_box = document.getElementsByClassName('orchestra');
+
+    for (i = 0; i < music.length; i++) {
+        if(!music[i].innerHTML.toLowerCase().includes(input)) {
+            music_box[i].style.display = "none";
+        } else {
+            music_box[i].style.display = "list-item";
+        }
+    }
+};
+
 /* Commented out, as I don't think this section is used any more.
 function expand() {
     var idList = ["e1", "e2"];
