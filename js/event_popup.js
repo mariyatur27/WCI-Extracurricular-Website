57a47d406e1fdaccdfc235bc19b9c1501933a7be
mariyatur27 <63456223+mariyatur27@users.noreply.github.com>
2021-12-09 20:26:47 -0500

    Tred to find a way to connect a JSON file to Brian's js, but didn't find one that works yet

--- js/event_popup.js
+++ js/event_popup.js
@@ -1,27 +1,33 @@
-function loadPopupData(){
-	if (event_popup == {}){
-		return;
-		// If there is no upcoming event, don't bother
-		// with this function and don't make the div
-		// visible, so it never shows up
-	}
-	var div = document.getElementById("event-popup");
-	var txt = document.getElementById("event-popup-text");
-	var icon = document.getElementById("event-popup-icon");
-	div.style.display = "flex";
-	txt.innerHTML = event_popup["text"];
-	div.style.backgroundColor = event_popup["color"];
-	icon.src = event_popup["icon"];
-	console.log(event_popup["icon"]);
-	console.log(icon.src);
-	if (event_popup["icon"] == ""){
-		icon.style.display = "none";
-		txt.style.left = "8px";
-		// If there is no image to show, move the text over to the left to avoid an awkward gap
-	}
-};
+	function loadPopupData(){
+		if (event_popup == {}){
+			return;
+		}
+		else {
+		fetch("data/event_popup.json")
+		.then(response => {
+			return response.json();
+		})
+		.then(data => console.log(data));
+		var div = document.getElementById("event-popup");
+		var txt = document.getElementById("event-popup-text");
+		var icon = document.getElementById("event-popup-icon");
+		div.style.display = "flex";
+		txt.innerHTML = event_popup.text;
+		div.style.backgroundColor = event_popup.color;
+		icon.src = event_popup.icon;
 
-function closePopup(){
-	var div = document.getElementById("event-popup");
-	div.style.display = "none";
-}
+		console.log(event_popup["icon"]);
+		console.log("sjfaifaiudhfaiuf")
+		console.log(icon.src);
+		if (event_popup["icon"] == ""){
+			icon.style.display = "none";
+			txt.style.left = "8px";
+			// If there is no image to show, move the text over to the left to avoid an awkward gap
+		}
+	  }
+	};
+
+	function closePopup(){
+		var div = document.getElementById("event-popup");
+		div.style.display = "none";
+	}
