09373c4a15167c55bb2c61b5b47fe34ecc47a191
awenelo <56133692+awenelo@users.noreply.github.com>
2021-12-17 16:39:03 -0500

    Attempt to create events based on seperate files

---
+++ data/assemble_data/assemble_events.py
@@ -0,0 +1,32 @@
+from yaml import safe_load as yaml_load
+from datetime import datetime
+from os import listdir
+from os.path import join as path_join
+from hashlib import sha1
+from json import dump as json_dump
+
+events = []
+eventDirectory = "data/events"
+
+# TODO: make resilient to typos
+for eventFilename in listdir(eventDirectory):
+    if not (eventFilename[-4:] == ".yml" or eventFilename[-5:] == ".yaml"):
+        continue
+    with open(path_join(eventDirectory, eventFilename)) as eventFile:
+        event = yaml_load(eventFile)
+
+    startDate = datetime.strptime(event["start-date"] + "." + event["start-time"], "%d/%m/%Y.%H:%M:%S")
+    endDate = datetime.strptime(event["end-date"] + "." + event["end-time"], "%d/%m/%Y.%H:%M:%S")
+    startTime = int(startDate.timestamp())
+    endTime = int(startDate.timestamp())
+
+    events.append({
+        "start_time": startTime,
+        "end_time": endTime,
+        "title": event["name"],
+        "description": event["description"],
+        "id": sha1(bytes(event["name"] + str(startTime) + str(endTime), 'utf-8')).hexdigest()
+        })
+
+with open("data/events.json", "w") as eventJSON:
+    json_dump(events, eventJSON, indent=4)
