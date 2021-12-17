abf2dce88aaf5917ca7659fbd1d108f6ee8ececb
mariyatur27 <63456223+mariyatur27@users.noreply.github.com>
2021-12-16 20:41:37 -0500

    art gallery, not done yet

--- js/art_gallery.js
+++ js/art_gallery.js
@@ -7,9 +7,9 @@ function dissappear(y) {
    y.style.color = "transparent";
    y.style.transition = "0.5s";
 }
-function dark(x) {
-   x.style.filter = "brightness(60%)";
-}
-function light(x) {
-   x.style.filter = "brightness(100%)";
-}
+// function dark(x) {
+//    x.style.filter = "brightness(60%)";
+// }
+// function light(x) {
+//    x.style.filter = "brightness(100%)";
+// }
