e31eba69c5a814d21da1625696323d1fb2f53100
awenelo <56133692+awenelo@users.noreply.github.com>
2021-12-29 17:04:37 -0500

    Don't error on missing fields in athletics yml files Also support extra_info

--- data/assemble_data/assemble_sports.py
+++ data/assemble_data/assemble_sports.py
@@ -6,7 +6,6 @@ from json import dump as json_dump
 athletics = []
 sportDirectory = "data/athletics"
 
-# TODO: make resilient to typos and missing data
 for sportFilename in listdir(sportDirectory):
     if not (sportFilename[-4:] == ".yml" or sportFilename[-5:] == ".yaml"):
         continue
@@ -15,17 +14,18 @@ for sportFilename in listdir(sportDirectory):
 
     connectionLinks = ["more_info"]
     connectionLinkVals = {}
-
-    for connectionLink in sport["connection-links"]:
-        connectionLinks.append(connectionLink["type"])
-        connectionLinkVals[connectionLink["type"]] = connectionLink["value"]
+    if "connection-links" in sport:
+        for connectionLink in sport["connection-links"]:
+            connectionLinks.append(connectionLink["type"])
+            connectionLinkVals[connectionLink["type"]] = connectionLink["value"]
 
     # TODO: Accuire button-type links automatically
     connectionLinks.sort(key=lambda link: (1 if link in ["website", "more_info"] else 0))
     
     final_dict = dict({"connection_links": connectionLinks}, **connectionLinkVals)
-    for originalLabel, newLabel in {"name": "name", "image": "image", "coach": "coach", "description": "description", "practice-time": "meeting_time", "practice-time-label": "meeting_time_title", "categories": "categories"}.items():
-        final_dict[newLabel] = sport[originalLabel]
+    for originalLabel, newLabel in {"name": "name", "image": "image", "coach": "coach", "description": "description", "practice-time": "meeting_time", "practice-time-label": "meeting_time_title", "categories": "categories", "extra-info": "extra_info"}.items():
+        if originalLabel in sport:
+            final_dict[newLabel] = sport[originalLabel]
 
     # TODO: Add team members
     athletics.append(final_dict)
