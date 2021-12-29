4420a700806393c6be9b0cab6a7448b3762fd8f8
awenelo <56133692+awenelo@users.noreply.github.com>
2021-12-29 17:31:20 -0500

    Support team members in athletics yaml files

--- data/assemble_data/assemble_sports.py
+++ data/assemble_data/assemble_sports.py
@@ -22,12 +22,25 @@ for sportFilename in listdir(sportDirectory):
     # TODO: Accuire button-type links automatically
     connectionLinks.sort(key=lambda link: (1 if link in ["website", "more_info"] else 0))
     
-    final_dict = dict({"connection_links": connectionLinks}, **connectionLinkVals)
+    final_dict = dict({"connection_links": connectionLinks, "id": ".".join(sportFilename.split(".")[:-1])}, **connectionLinkVals)
     for originalLabel, newLabel in {"name": "name", "image": "image", "coach": "coach", "description": "description", "practice-time": "meeting_time", "practice-time-label": "meeting_time_title", "categories": "categories", "extra-info": "extra_info"}.items():
         if originalLabel in sport:
             final_dict[newLabel] = sport[originalLabel]
+    if "team-members" in sport:
+        teamMembers = []
+        showImages = "team-member-image-folder" in sport
+        for teamMemberName in sport["team-members"]:
+            teamMember = {"name": teamMemberName}
+            if showImages:
+                teamMember["image"] = sport["team-member-image-folder"] + teamMemberName + ".png"
+            teamMembers.append(teamMember)
+        final_dict["members"] = teamMembers
+    elif "team-images" in sport and "team-member-image-folder" in sport:
+        teamMembers = []
+        for image in sport["team-images"]:
+            teamMembers.append({"image": sport["team-member-image-folder"] + image + ".png"})
+        final_dict["members"] = teamMembers
 
-    # TODO: Add team members
     athletics.append(final_dict)
 
 with open("data/athletics.json", "w") as sportsJSON:
