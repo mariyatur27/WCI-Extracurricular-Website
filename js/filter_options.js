91495ac56d5112fb1ab1c5e0a5acc75766f108cd
awenelo <56133692+awenelo@users.noreply.github.com>
2022-01-02 17:22:49 -0500

    Improve filter filtering, expanding & collapsing

--- js/filter_options.js
+++ js/filter_options.js
@@ -6,58 +6,54 @@ function toggleFilter(filter) {
     }
 }
 
-function toggleFilterActive(filter) {
-    var filterWrapper = document.getElementById(filter).parentElement;
-    filterWrapper.classList.toggle("filter-active");
+function toggleFilterActive(filterElement) {
+    filterElement.parentElement.classList.toggle("filter-active");
 }
 
-function toggleClubFilter(filter) {
+function toggleClubFilter(filterElement) {
+    let filter = filterElement.id;
     toggleFilter(filter + "_category");
-    toggleFilterActive(filter);
+    toggleFilterActive(filterElement);
     setupBoxes(clubs, 'club_boxes', 2);
 }
 
-function toggleMusicFilter(filter) {
+function toggleMusicFilter(filterElement) {
+    let filter = filterElement.id;
     toggleFilter(filter);
-    toggleFilterActive(filter);
+    toggleFilterActive(filterElement);
     setupBoxes(music, 'music_boxes', 2);
 }
 
-function toggleAthleticsFilter(filter) {
+function toggleAthleticsFilter(filterElement) {
+    let filter = filterElement.id;
     toggleFilter(filter);
-    toggleFilterActive(filter);
+    toggleFilterActive(filterElement);
     setupBoxes(athletics, 'athletics_boxes', 2);
 }
 
-function toggleDropdown(dropdown_content) {
-    dropdown_content.classList.toggle('dropdown-active');
+function toggleDropdown(dropdown_button) {
+    hideDropdown();
+    dropdown_button.getElementsByClassName('dropdown_content')[0].classList.toggle('dropdown-active');
 }
 
 function hideDropdown() {
-    var dropdown_content1 = document.getElementById('dropdown_content1');
-    var dropdown_content2 = document.getElementById('dropdown_content2');
-    var dropdown_content3 = document.getElementById('dropdown_content3');
-    var dropdown_content4 = document.getElementById('dropdown_content4');
-    dropdown_content1.classList.remove('dropdown-active');
-    dropdown_content2.classList.remove('dropdown-active');
-    dropdown_content3.classList.remove('dropdown-active');
-    dropdown_content4.classList.remove('dropdown-active');
+    for (const dropdown of document.getElementsByClassName('dropdown_content')) {
+        dropdown.classList.remove('dropdown-active');
+    }
 }
 
 window.onclick = function (event) {
     // if the user clicks anywhere outside of the filters dropdown menu, hide menu
     // check if the id, className, or tagName of event.target matches any of the elements of the dropdown menu
-    var exceptions = ["filterbt", "dropdown-btn", "LABEL", "dropdown_content"];
+    var exceptions = ["filterbt", "dropdown-btn", "LABEL", "dropdown_content", "dropdown"];
     var hide = true;
     for (let exception of exceptions) {
         var className = event.target.classList.value;
         var id = event.target.id;
         var tagName = event.target.tagName;
-        if (className == exception || id == exception || tagName == exception) {
-            hide = false;
+        if (className.includes(exception) || exception == id || exception == tagName) {
+            return;
         }
     }
-    if (hide) {
-        hideDropdown();
-    }
+    hideDropdown();
 }
