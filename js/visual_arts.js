194a81bb71d220a74fab4d05deaa39b5e084cd13
mariyatur27 <63456223+mariyatur27@users.noreply.github.com>
2022-01-25 12:39:07 -0500

    made the gallery dynamic

--- js/visual_arts.js
+++ js/visual_arts.js
@@ -1,15 +1,19 @@
 async function buildGallery(galleryID) {
     if (!dataFetched) {
-        await fetch_data();
+        await fetchData();
     }
 
     let section = document.getElementById("the_gallery");
     
     for (const art of gallery) {
-        if (art.id == galleryID) {
             console.log(art);
             let box = document.createElement("div"); box.classList.add("box");
-                let imgDiv = document.createElement("div"); imgDiv.classList.add("img-box");
+                let imgDiv = document.createElement("div");
+                if ("class" in art){
+                    imgDiv.classList.add("img-box"); imgDiv.classList.add(art.class);
+                }else{
+                    imgDiv.classList.add("img-box");  
+                }
                     let image = document.createElement("img"); image.src = art.image;
                     imgDiv.appendChild(image);
                 box.appendChild(imgDiv);
@@ -27,6 +31,4 @@ async function buildGallery(galleryID) {
                 box.appendChild(info);
             section.appendChild(box);
         }
-
-    }
 }
